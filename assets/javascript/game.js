9eb6cc24c671b7f8c96e52c5fb32bbf60b2537f2
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-03-29 22:52:25 -0600

    added coundown timer logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -129,3 +129,52 @@ $(document).ready(function () {
 
       $(".subBtn").empty();
   });
+
+  function start() {
+    if (!clockRunning) {
+      intervalId = setInterval(count, 1000);
+      clockRunning = true;
+    }
+  
+  }
+  function stop() {
+    clearInterval(intervalId);
+    clockRunning = false;
+  }
+
+  function timeOut() {
+    console.log(time);
+    if (time === 0) {
+        $(".questionClass").empty();
+        clearInterval(intervalId);
+      
+    }
+  }
+
+  function count() {
+    time--;
+    var convertedTime = timeConverter(time);
+    $("#display").html(convertedTime);
+    if (time === 0) {
+        //$(".questionClass").empty(); // Empty removed the checkboxes, therefore ALL were undefined or Unanswered
+        $('input[type=radio]').attr('disabled','true');
+        clearInterval(intervalId);
+      
+    }
+  }
+
+  function timeConverter(t) {
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    if (minutes === 0) {
+      minutes = "00";
+    }
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    return minutes + ":" + seconds;
+  } 
+}); 
