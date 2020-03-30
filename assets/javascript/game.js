e255c3f0b1a1e2644ecf23d355e615a419b9bdde
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-03-29 22:51:03 -0600

    added questions with radio buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,4 +7,49 @@ $(document).ready(function () {
     var intervalId;
     var clockRunning = false;
     var time = 30;
+
+    //Adds button to begin game
+    $("#startBtn").addClass("mt-3 lead beginGame");
+    $(".beginGame").append("<button>Start Game</button>");
+  //
+    $("#startBtn").on("click", function() {
+      $(".beginGame").empty();
+      $("#begin").addClass("mt-3 questionClass");
+      $("#begin").append('<p class="mt-3 questionClass">In New Vegas: </p>');
+      
+      start();
+      timeOut();
+      
+          //Question 1
+      $("#questionOne").addClass("mt-3 ml-3 q1 questionClass");
+      $(".q1").append("<p>You start the game with a rocket launcher.</p>");
   
+      $(".q1").append("<label><input type='radio' name='qOne' value='true'> True </label> <label><input type='radio' name='qOne' value='false'> False </label>");
+  
+          //Question 2
+      $("#questionTwo").addClass("mt-3 ml-3 q2 questionClass");
+      $(".q2").append("<p>You can vist Nellis Air Force Base.</p>");
+  
+      $(".q2").append("<label><input type='radio' name='qTwo' value='true'> True </label> <label><input type='radio' name='qTwo' value='false'> False </label>");
+  
+          //Question 3
+      $("#questionThree").addClass("mt-3 ml-3 q3 questionClass");
+      $(".q3").append("<p>You are a farmer before the adventure starts.</p>");
+  
+      $(".q3").append("<label><input type='radio' name='qThree' value='true'> True </label> <label><input type='radio' name='qThree' value='false'> False </label>");
+  
+          //Question 4
+      $("#questionFour").addClass("mt-3 ml-3 q4 questionClass");
+      $(".q4").append("<p>Deathclaw is the name of the dog you can be friends with.</p>");
+  
+      $(".q4").append("<label><input type='radio' name='qFour' value='true'> True </label> <label><input type='radio' name='qFour' value='false'> False </label>");
+  
+          //Question 5
+      $("#questionFive").addClass("mt-3 ml-3 q5 questionClass");
+      $(".q5").append("<p>Benny shoots you in the head.</p>");
+  
+      $(".q5").append("<label><input type='radio' name='qFive' value='true'> True </label> <label><input type='radio' name='qFive' value='false'> False </label>");
+  
+          //Submit Answers
+      $("#submitBtn").append("<p><input type='button' value='Submit'></p>").addClass("pb-5 pt-4 quest subBtn");
+    });
