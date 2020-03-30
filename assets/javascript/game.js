d8767cc237e1cf08cfff5b6af68c3d602101de49
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-03-29 22:49:03 -0600

    created variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  //Counters for answers
+    var correct = 0;
+    var wrong = 0;
+    var unanswered = 0;
+  //Timer Var
+    var intervalId;
+    var clockRunning = false;
+    var time = 30;
+  
